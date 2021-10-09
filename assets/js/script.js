0286de68e1fd7d2c89319bd9e1da1cda388f7c5f
lexach91 <konovalov1991@mail.ru>
2021-10-09 19:59:05 +0800

    Add comments to the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-dc.config.defaultColors(d3.schemeCategory10);
+dc.config.defaultColors(d3.schemeCategory10); //Changing default DC.js color scheme
+
+// Creating variables for charts
 const mapChart = dc.geoChoroplethChart("#us-map");
 const weatherChart = dc.pieChart("#weather-selector");
 const timelineChart = dc.barChart("#timeline");
@@ -11,23 +13,27 @@ const topCitiesChart = dc.rowChart("#top-cities");
 const dataTable = dc.dataTable("#data-table");
 const dayOfWeekChart = dc.rowChart("#day-of-week-chart");
 
-const dataUrl = "https://query.data.world/s/5c54uvvfqkeg6vtct5tquymi4nbe7f";
-// 2016-02-08 00:37:08
+const dataUrl = "https://query.data.world/s/5c54uvvfqkeg6vtct5tquymi4nbe7f"; // Link to dataset hosted at data.world
+
+// Variables for parsing and formatting date
 const parseDate = d3.timeParse("%Y-%m-%d");
 const formatDate = d3.timeFormat("%Y-%m-%d");
 
-d3.csv(dataUrl)
+d3.csv(dataUrl) // Fetching the dataset by creating a promise with D3 library
   .catch((err) => {
-    document.getElementById("error").style.display = "block";
+    document.getElementById("error").style.display = "block"; //If we catch an error while fetching data, an error will be shown to user
     throw err;
   })
   .then((data) => {
     data.forEach((d) => {
-      d.date = parseDate(d.date.slice(0,10));
+      d.date = parseDate(d.date.slice(0, 10)); //Parsing a date from a date column in dataset
     });
+
+    //Adding variables for all data with crossfilter library
     const csData = crossfilter(data);
     const all = csData.groupAll();
 
+    // Creating dimensions that will be used for building charts
     const stateDim = csData.dimension(dc.pluck("state"));
     const dateDim = csData.dimension(dc.pluck("date"));
     const weatherDim = csData.dimension(dc.pluck("weather_condition"));
@@ -36,12 +42,21 @@ d3.csv(dataUrl)
     const statesDim = csData.dimension(dc.pluck("state"));
     const countiesDim = csData.dimension(dc.pluck("county"));
     const citiesDim = csData.dimension(dc.pluck("city"));
-    const dayOfWeekDim = csData.dimension(d => {
+    const dayOfWeekDim = csData.dimension((d) => {
       const day = d.date.getDay();
-      const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
       return week[day];
     });
 
+    // Grouping all dimensions
     const accidentsByStateGroup = stateDim.group();
     const weatherGroup = weatherDim.group();
     const dateGroup = dateDim.group();
@@ -52,8 +67,9 @@ d3.csv(dataUrl)
     const citiesGroup = citiesDim.group();
     const dayOfWeekGroup = dayOfWeekDim.group();
 
-
+    // Another d3 promise to load geojson file for building a map of USA
     d3.json("assets/data/us-states.json").then((mapJson) => {
+      // Map chart
       mapChart
         .height(500)
         .width(870)
@@ -73,6 +89,7 @@ d3.csv(dataUrl)
         })
         .useViewBoxResizing(true);
 
+      // Weather selector chart
       weatherChart
         .height(350)
         .width(550)
@@ -93,6 +110,7 @@ d3.csv(dataUrl)
         })
         .useViewBoxResizing(true);
 
+      //Top 10 states by amount of accidents chart
       topStatesChart
         .height(700)
         .width(900)
@@ -112,6 +130,7 @@ d3.csv(dataUrl)
         .xAxis()
         .ticks(6);
 
+      //Top 10 counties by amount of accidents chart
       topCountiesChart
         .height(700)
         .width(900)
@@ -131,6 +150,7 @@ d3.csv(dataUrl)
         .xAxis()
         .ticks(6);
 
+      //Top 10 cities by amount of accidents chart
       topCitiesChart
         .height(700)
         .width(900)
@@ -150,6 +170,7 @@ d3.csv(dataUrl)
         .xAxis()
         .ticks(6);
 
+      // Timeline chart
       timelineChart
         .width(1300)
         .height(300)
@@ -168,11 +189,13 @@ d3.csv(dataUrl)
         .xAxis()
         .ticks(30);
 
+      //Total number of selected accidents
       totalNumber
         .group(all)
         .valueAccessor((x) => x)
         .formatNumber(d3.format("d"));
 
+      // Day-night selector chart
       dayNightChart
         .height(250)
         .width(300)
@@ -185,9 +208,10 @@ d3.csv(dataUrl)
             (d.value ? d.value : 0)
           );
         })
-          .group(dayNightGroup)
-          .useViewBoxResizing(true);
+        .group(dayNightGroup)
+        .useViewBoxResizing(true);
 
+      // Severity of an impact on traffic chart
       severityChart
         .height(300)
         .width(1000)
@@ -215,6 +239,7 @@ d3.csv(dataUrl)
         })
         .useViewBoxResizing(true);
 
+      // Table on aggregated data
       dataTable
         .dimension(dateDim)
         .showSections(false)
@@ -230,40 +255,42 @@ d3.csv(dataUrl)
           "state",
           "county",
           "city",
-          "description"
+          "description",
         ])
         .useViewBoxResizing(true);
 
-        dayOfWeekChart
-          .height(400)
-          .width(1000)
-          .dimension(dayOfWeekDim)
-          .group(dayOfWeekGroup)
-          .elasticX(true)
-          .ordering((d) => {
-            const order = {
-              Sunday: 0,
-              Monday: 1,
-              Tuesday: 2,
-              Wednesday: 3,
-              Thursday: 4,
-              Friday: 5,
-              Saturday: 6,
-            };
-            return order[d.key];
-          })
-          .title(function (d) {
-            return (
-              "Day of a week: " +
-              d.key +
-              "\nNumber of accidents: " +
-              (d.value ? d.value : 0)
-            );
-          })
-          .useViewBoxResizing(true);
+      // Day of a week chart
+      dayOfWeekChart
+        .height(400)
+        .width(1000)
+        .dimension(dayOfWeekDim)
+        .group(dayOfWeekGroup)
+        .elasticX(true)
+        .ordering((d) => {
+          const order = {
+            Sunday: 0,
+            Monday: 1,
+            Tuesday: 2,
+            Wednesday: 3,
+            Thursday: 4,
+            Friday: 5,
+            Saturday: 6,
+          };
+          return order[d.key];
+        })
+        .title(function (d) {
+          return (
+            "Day of a week: " +
+            d.key +
+            "\nNumber of accidents: " +
+            (d.value ? d.value : 0)
+          );
+        })
+        .useViewBoxResizing(true);
 
-      dc.renderAll();
+      dc.renderAll(); //Function that draws all charts
 
+      // Hiding the loader and displaying the page after all charts are drawn
       document.getElementById("loader").style.display = "none";
       document.getElementsByClassName("container")[0].style.display = "block";
       document.getElementsByTagName("footer")[0].style.display = "flex";
