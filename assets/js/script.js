a8dad669139b6bc3e8925a2798d0257392157c52
lexach91 <konovalov1991@mail.ru>
2021-10-21 17:46:37 +0800

    Fix positioning of headers and modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ dc.config.defaultColors(d3.schemeCategory10); //Changing default DC.js color sch
 
 function toggleModal(event) {
   event.preventDefault();
-  let modal = event.target.parentNode.parentNode.getElementsByClassName("modal")[0];
+  let modal = event.target.parentNode.getElementsByClassName("modal")[0];
   let button = modal.getElementsByTagName("button")[0];
   if (modal.style.display === "flex") {
     modal.style.display = "none";
@@ -120,7 +120,7 @@ d3.csv(dataUrl) // Fetching the dataset by creating a promise with D3 library
         .dimension(weatherDim)
         .group(weatherGroup)
         .data((group) => group.top(10)) // Shows only top 10 weather conditions with most accidents on applied filters
-        .legend(dc.legend())
+        .legend(dc.legend().x(20).y(100).itemHeight(13).gap(5))
         .innerRadius(50) // Make pie chart into a donut chart
         .minAngleForLabel(100) // Hide labels from the chart, because we use a legend
         .cx(350) // Translating the center of the chart to the right to make it more centered with legend
